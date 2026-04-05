cc4f4b7e7a3dc45253920458c19a97553a0260d9
zsluss <zluss@gmail.com>
2026-04-05 11:33:04 -0500

    attempt2 at remove

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -151,7 +151,7 @@ template <class T>
 TreeNode<T> *BinarySearchTree<T>::search(T &data) const
 {
     TreeNode<T> *currentNode = root;
-    //going to keep going through the tree until we find the data or reach a null pointer, if we find the data, we return the node, if we reach a null pointer, we return null
+    // going to keep going through the tree until we find the data or reach a null pointer, if we find the data, we return the node, if we reach a null pointer, we return null
     while (currentNode != nullptr)
     {
         if (data == currentNode->data) // if we find the data, we return the node
@@ -160,15 +160,15 @@ TreeNode<T> *BinarySearchTree<T>::search(T &data) const
         }
         else if (data < currentNode->data) // if the data is less than the current node's data, we go left, if not we go right
         {
-            currentNode = currentNode->left; 
+            currentNode = currentNode->left;
         }
-        else //if its not less than or equal then it has to go right
+        else // if its not less than or equal then it has to go right
         {
             currentNode = currentNode->right;
         }
     }
     return nullptr; // if it can't find the searched number then it returns null
-}   
+}
 
 template <class T>
 void BinarySearchTree<T>::insert(T &data)
@@ -193,7 +193,7 @@ void BinarySearchTree<T>::insert(T &data)
             // checking to see if the data is less than the current node's data, if it is, we go left, if not we go right
             if (data < currentNode->data)
             {
-                //checking if the left is open (null), if it is, we insert the new node there, if not we keep traversing left
+                // checking if the left is open (null), if it is, we insert the new node there, if not we keep traversing left
                 if (currentNode->left == nullptr)
                 {
                     currentNode->left = new TreeNode<T>;
@@ -208,7 +208,7 @@ void BinarySearchTree<T>::insert(T &data)
             }
             else
             {
-                //checking if the right is open (null), if it is, we insert the new node there, if not we keep traversing right
+                // checking if the right is open (null), if it is, we insert the new node there, if not we keep traversing right
                 if (currentNode->right == nullptr)
                 {
                     currentNode->right = new TreeNode<T>;
@@ -227,7 +227,7 @@ void BinarySearchTree<T>::insert(T &data)
 
 template <class T>
 TreeNode<T> *BinarySearchTree<T>::successor(TreeNode<T> *node) const
-{  
+{
     if (node == nullptr)
         return nullptr;
     // If the node has a right child, the successor is the leftmost node in the right subtree
@@ -254,26 +254,199 @@ TreeNode<T> *BinarySearchTree<T>::successor(TreeNode<T> *node) const
 template <class T>
 bool BinarySearchTree<T>::remove(T &data)
 {
-//not ready to work on this
-return false;
+    TreeNode<T> *parent = nullptr;
+    TreeNode<T> *currentNode = root;
+    while (currentNode != nullptr)
+    {
+        // Check if currentNode has an equal data
+        if (currentNode->data == data)
+        {
+            if (currentNode->left == nullptr && currentNode->right == nullptr)
+            {
+                // Remove leaf
+
+                if (isRoot(currentNode))
+                { // Node is root
+                    root = nullptr;
+                }
+                else if (parent->left == currentNode)
+                {
+                    parent->left = nullptr;
+                }
+                else
+                {
+                    parent->right = nullptr;
+                }
+                delete currentNode;
+                return true; // Node found and removed
+            }
+            else if (currentNode->right == nullptr)
+            {
+                // Remove node with only left child
+
+                if (isRoot(currentNode))
+                { // Node is root
+                    root = currentNode->left;
+                }
+                else if (parent->left == currentNode)
+                {
+                    parent->left = currentNode->left;
+                }
+                else
+                {
+                    parent->right = currentNode->left;
+                }
+                delete currentNode;
+                return true; // Node found and removed
+            }
+            else if (currentNode->left == nullptr)
+            {
+                // Remove node with only right child
+
+                if (isRoot(currentNode))
+                { // Node is root
+                    root = currentNode->right;
+                }
+                else if (parent->left == currentNode)
+                {
+                    parent->left = currentNode->right;
+                }
+                else
+                {
+                    parent->right = currentNode->right;
+                }
+                delete currentNode;
+                return true; // Node found and removed
+            }
+            else
+            {
+                // Remove node with two children
+
+                TreeNode<T> *foundSuccessor = successor(currentNode);
+
+                // Copy successor's data to current node
+                currentNode->data = foundSuccessor->data;
+                parent = currentNode;
+
+                // Reassign currentNode and data so that loop continues with new data
+                currentNode = currentNode->right;
+                data = foundSuccessor->data;
+            }
+        }
+        else if (currentNode->data < data)
+        {
+            // Search right
+            parent = currentNode;
+            currentNode = currentNode->right;
+        }
+        else
+        {
+            // Search left
+            parent = currentNode;
+            currentNode = currentNode->left;
+        }
+    }
+    return false; // Node not found
 }
+
+/*
+//not ready to work on this
+if (root == nullptr)
+{
+    return false; // Tree is empty, nothing to remove
+}
+
+TreeNode<T> *nodeToRemove = search(data);
+if (nodeToRemove == nullptr)
+{
+    return false; // Data not found in the tree
+}
+if (isRoot(nodeToRemove) && isLeaf(nodeToRemove)) // if the node to remove is the root and a leaf,just set the root to null and delete the node
+{
+    delete root;
+    root = nullptr;
+    return true;
+}
+if(isLeaf(nodeToRemove)) // if the node to remove is a leaf, we can just delete it and set its parent's pointer to null
+{
+    if (isLeft(nodeToRemove))
+    {
+        nodeToRemove->parent->left = nullptr;
+    }
+    else
+    {
+        nodeToRemove->parent->right = nullptr;
+    }
+    delete nodeToRemove;
+    return true;
+}
+if(nodeToRemove->left != nullptr && nodeToRemove->right != nullptr) // if the node to remove has two children, we need to find its successor, replace the node's data with the successor's data, and then remove the successor node
+{
+    TreeNode<T> *successorNode = successor(nodeToRemove);
+    //check if succesor left is null then it can just become the node to remove with the same parent and right child
+    if(successorNode->left == nullptr)
+    {
+        successorNode->left = nodeToRemove->left;
+        successorNode->parent = nodeToRemove->parent;
+        if (isRoot(nodeToRemove))
+        {
+            root = successorNode;
+        }
+        else if (isLeft(nodeToRemove))
+        {
+            nodeToRemove->parent->left = successorNode;
+        }
+        else
+        {
+            nodeToRemove->parent->right = successorNode;
+        }
+        delete nodeToRemove;
+        return true;
+    }
+
+
+
+}
+if (nodeToRemove->left != nullptr || nodeToRemove->right != nullptr) // if the node to remove has one child, we need to replace the node with its child
+{
+    TreeNode<T> *childNode = (nodeToRemove->left != nullptr) ? nodeToRemove->left : nodeToRemove->right;
+    if (isRoot(nodeToRemove))
+    {
+        root = childNode;
+    }
+    else if (isLeft(nodeToRemove))
+    {
+        nodeToRemove->parent->left = childNode;
+    }
+    else
+    {
+        nodeToRemove->parent->right = childNode;
+    }
+    childNode->parent = nodeToRemove->parent;
+    delete nodeToRemove;
+    return true;
+}
+}
+
+*/
 
 template <class T>
 bool BinarySearchTree<T>::isEmpty() const
 {
-    return root == nullptr;
+return root == nullptr;
 }
 
 template <class T>
 int BinarySearchTree<T>::height() const
 {
-    return treeHeight(root);
+return treeHeight(root);
 }
 
 template <class T>
 int BinarySearchTree<T>::size() const
 {
-    return treeSize(root);
+return treeSize(root);
 }
+
 
 #endif /* BinarySearchTree_h */
